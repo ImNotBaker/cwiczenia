b473747a95ee2bfb406c91ea8d8cff7b976a6eb6
Jakub Baker <tetranax@outlook.com>
2018-04-27 08:55:17 +0200

    Zmiany w trakcie ćwiczeń

---
+++ spiewanie.py
@@ -0,0 +1,15 @@
+def happybirthday(nazwa):
+    print("Happy birthday to you")
+    print("Happy birthday to you")
+    print("Happy birthday, dear  " + nazwa + " . ")
+    print("Happy birthday to you")
+
+
+def main():
+    #pytanie o imie
+    imie = input ('Podaj imie:  ')
+    happybirthday(imie)
+
+main()
+
+
---
+++ text1.py
@@ -0,0 +1,6 @@
+def potega3(x):
+    return x*x*x
+def main():
+    print(potega3(2))
+
+main()
